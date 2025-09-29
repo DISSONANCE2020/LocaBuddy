2c4fddc262691c3c124036038b93b853e46d79fd
DISSONANCE2020 <132447240+DISSONANCE2020@users.noreply.github.com>
2025-09-29 15:47:18 +0800

    sheetTopper at BottomPullable Updated

--- src/components/HomeScreen/BottomPullable/BottomPullable.styles.ts
+++ src/components/HomeScreen/BottomPullable/BottomPullable.styles.ts
@@ -1,27 +1,28 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  
   sheet: {
     position: "absolute",
     left: 0,
     right: 0,
     bottom: 0,
     backgroundColor: "#fff",
-    borderTopLeftRadius: 16,
-    borderTopRightRadius: 16,
-    paddingTop: 12,
-    paddingHorizontal: 16,
+    borderTopLeftRadius: 32,
+    borderTopRightRadius: 32,
     elevation: 10,
+  },
+  sheetTopper: {
+    borderRadius: 32,
+    height: 120,
+    padding: 16,
   },
   handle: {
     width: 40,
     height: 4,
     borderRadius: 2,
-    backgroundColor: "#ccc",
+    backgroundColor: "#ffffffff",
     alignSelf: "center",
     marginBottom: 8,
-    marginTop: 8,
   },
   content: {
     flex: 1,
@@ -29,5 +30,52 @@ export default StyleSheet.create({
     justifyContent: "center",
     padding: 16,
     minHeight: 100,
+  },
+
+  // sheetTopper content
+
+  sheetTopperContent: {
+    flexDirection: "row",
+  },
+
+  avatar: {
+    width: 64,
+    height: 64,
+    borderRadius: 100,
+    backgroundColor: "#ffffffff",
+  },
+  info: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    marginLeft: 12,
+    flex: 1,
+  },
+  username: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: "#ffffffff",
+  },
+  locationDetails: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
+  locationText: {
+    fontSize: 12,
+    fontWeight: "500",
+    marginRight: 8,
+    color: "#ffffffff",
+  },
+  expBarBackground: {
+    flex: 1,
+    height: 6,
+    backgroundColor: "#ddd",
+    borderRadius: 3,
+    overflow: "hidden",
+  },
+  expBarFill: {
+    width: "50%",
+    height: "100%",
+    backgroundColor: "#ffc400ff",
   },
 });
--- src/components/HomeScreen/BottomPullable/BottomPullable.tsx
+++ src/components/HomeScreen/BottomPullable/BottomPullable.tsx
@@ -5,8 +5,11 @@ import {
   Text,
   Dimensions,
   Animated,
+  Image,
   PanResponder,
 } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
 import styles from "./BottomPullable.styles";
 
 const { height } = Dimensions.get("window");
@@ -95,7 +98,31 @@ export default function BottomPullable({
         style={[styles.sheet, { height: animatedHeight }]}
         {...panResponder.panHandlers}
       >
-        <View style={styles.handle} />
+        <LinearGradient
+          colors={["#11998E", "#38EF7D"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={styles.sheetTopper}
+        >
+          <View style={styles.handle} />
+          <View style={styles.sheetTopperContent}>
+            <Image
+              source={{
+                uri: "https://www.vectorstock.com/royalty-free-vector/profile-placeholder-image-gray-silhouette-vector-21542863",
+              }}
+              style={styles.avatar}
+            />
+            <View style={styles.info}>
+              <Text style={styles.username}>Gab</Text>
+              <View style={styles.locationDetails}>
+                <Text style={styles.locationText}>
+                  At Sor Luisa St, Angeles, 2009 Pampanga
+                </Text>
+                <Text style={styles.locationText}>Since 2:30 PM</Text>
+              </View>
+            </View>
+          </View>
+        </LinearGradient>
         <View style={styles.content}>
           <Text>{isExpanded ? "Expanded" : "Default"}</Text>
         </View>
