1a65fdc95cb197fb8781cad5d4b2c6b91c563424
DISSONANCE2020 <132447240+DISSONANCE2020@users.noreply.github.com>
2025-09-29 15:18:15 +0800

    Profile Header Updates STATIC FINAL

--- src/components/HomeScreen/ProfileHeader/ProfileHeader.tsx
+++ src/components/HomeScreen/ProfileHeader/ProfileHeader.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
 import styles from "./ProfileHeader.styles";
 
 export default function ProfileHeader() {
   return (
     <View style={styles.container}>
       <LinearGradient
-        colors={['#11998E', '#38EF7D']}
+        colors={["#11998E", "#38EF7D"]}
         start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 0 }} style={styles.content}>
+        end={{ x: 1, y: 0 }}
+        style={styles.content}
+      >
         <Image
           source={{
             uri: "https://www.vectorstock.com/royalty-free-vector/profile-placeholder-image-gray-silhouette-vector-21542863",
@@ -21,7 +23,12 @@ export default function ProfileHeader() {
           <View style={styles.levelRow}>
             <Text style={styles.levelText}>LVL 10</Text>
             <View style={styles.expBarBackground}>
-              <View style={styles.expBarFill} />
+              <LinearGradient
+                colors={["#FF7865", "#F4DC32"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                style={styles.expBarFill}
+              />
             </View>
           </View>
         </View>
