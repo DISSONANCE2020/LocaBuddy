591092e2d1b9144e60eb26db7c09476c9aa8f9fb
DISSONANCE2020 <132447240+DISSONANCE2020@users.noreply.github.com>
2025-10-14 14:44:32 +0800

    Removed @metamask/sdk-react-native dependency and added react-native-url-polyfill

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "react-native-get-random-values";
+import "react-native-url-polyfill/auto";
 import { WalletProvider } from "./src/lib/wallet";
 import LoginSignup from "./src/screens/LoginSignup/LoginSignup";
 
